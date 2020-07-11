24a427ee3cd55e76ad4b04f5c8655b78e186bc28
jungissei <issei_matsuhashi@yahoo.co.jp>
2020-07-11 17:20:28 +0900

    add model relation post and user

--- db/migrate/20200710131530_create_posts.rb
+++ db/migrate/20200710131530_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title
       t.string :content
+      t.text :image
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
