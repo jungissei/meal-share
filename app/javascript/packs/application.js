f343b14732c62e71f4e9704411a9e1b3e869195a
jungissei <issei_matsuhashi@yahoo.co.jp>
2020-12-20 15:40:13 +0900

    fix webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("jquery");
-
-import "../../../node_modules/bootstrap/dist/js/bootstrap.min"
-import "../../../node_modules/@fortawesome/fontawesome-free/js/all"
+import "../../../modules/jquery/dist/jquery"
+import "../../../modules/bootstrap/dist/js/bootstrap"
+import "../../../modules/@fortawesome/fontawesome-free/js/all"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
