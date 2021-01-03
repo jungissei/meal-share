14caff53a9e957c5b75b418040fc04c902baae98
jungissei <issei_matsuhashi@yahoo.co.jp>
2021-01-03 12:21:47 +0900

    fixed some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,30 +19,18 @@ class ApplicationController < ActionController::Base
     # Returns the value of the src attribute of
     # the img tag of the thumbnail size image.
     def thumb(image)
-      unless image.thumb.file.exists?
-        return image.thumb.url.to_s
-      end
-
-      image.url.to_s
+      image.thumb.url.to_s
     end
 
     # Returns the value of the src attribute of
     # the img tag for a medium-sized thumbnail image.
     def medium_thumb(image)
-      unless image.medium_thumb.file.exists?
-        return image.medium_thumb.url.to_s
-      end
-
-      image.url.to_s
+      image.medium_thumb.url.to_s
     end
 
     # Returns the value of the src attribute
     # of the img tag for a small-sized thumbnail image.
     def small_thumb(image)
-      unless image.small_thumb.file.exists?
-        return image.small_thumb.url.to_s
-      end
-
-      image.url.to_s
+      image.small_thumb.url.to_s
     end
 end
